27ca5dcfffd1f810ca957c8ca597946d253a432e
João Pedro Torrano Dias <joaopedrodias028@gmail.com>
2026-09-25 15:19:05 -0300

    exercicio 7

--- C11_Cap5/ex4.py
+++ C11_Cap5/ex4.py
@@ -10,3 +10,14 @@ descricao_populacao = populacao_regiao.describe()
 print(descricao_populacao)
 print(descricao_populacao.head(5))
 
+#7)
+def infantMred (x):
+    return x*0.85
+
+original = ds['Infant mortality (per 1000 births)']
+meta = original.apply(infantMred)
+meta.name = 'Infant mortality goal (-15%)'
+
+comparacao = pd.concat([original, meta], axis=1)
+print(comparacao)
+
