a7801e71853b566a7f53e06fbf63141ad7bb9fb0
João Pedro Torrano Dias <joaopedrodias028@gmail.com>
2026-09-25 14:43:28 -0300

    exercicio 4 e  5

--- C11_Cap5/ex3.py
+++ C11_Cap5/ex3.py
@@ -22,4 +22,22 @@ print('Número de países da Oceania:', paises_oceania.count())
 paises_regiao = ds.groupby('Region')
 print(paises_regiao['Literacy (%)'].sum() / paises_regiao['Literacy (%)'].count())
 
+#4)
+noCoast = ds[ds['Coastline (coast/area ratio)'] == 0]['Country']
+print(noCoast)
+noCoast.to_csv('noCoast.csv', index=False)
+
+#5)
+def deathInd (x):
+    if x < 9:
+        return 'Balanced'
+    else:
+        return 'Urgent'
+
+ds['Humanitarian Help'] = ds['Deathrate'].apply(deathInd)
+print(ds)
+
+
+
+
 
