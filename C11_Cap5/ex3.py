01bba2348a1293a18c7540675227fec07e54797f
João Pedro Torrano Dias <joaopedrodias028@gmail.com>
2026-09-25 14:00:02 -0300

    exercicio 1

--- C11_Cap5/ex3.py
+++ C11_Cap5/ex3.py
@@ -1,8 +1,21 @@
 import numpy as np
 import pandas as pd
 
-ds= pd.read_csv('paises.csv', sep = ';')
+ds = pd.read_csv('paises.csv', sep=';')
+
+#1)
+# Os valores de Region e Country vêm com espaços sobrando no fim
+ds['Region'] = ds['Region'].str.strip()
+ds['Country'] = ds['Country'].str.strip()
 
 print(ds.columns)
+
 paises_oceania = ds[ds['Region'] == 'OCEANIA']['Country']
 print(paises_oceania)
+
+print('Número de países da Oceania:', paises_oceania.count())
+
+#2)
+
+
+
