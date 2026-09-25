63ee486f91b5533fbd5fe204289c31f91dd02e6e
João Pedro Torrano Dias <joaopedrodias028@gmail.com>
2026-09-25 15:00:03 -0300

    exercicio 6

--- C11_Cap5/ex4.py
+++ C11_Cap5/ex4.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
 
+ds = pd.read_csv('paises.csv', sep = ';')
+
+#6)
+paises_regiao = ds.groupby('Region')
+populacao_regiao = paises_regiao['Population']
+descricao_populacao = populacao_regiao.describe()
+print(descricao_populacao)
+print(descricao_populacao.head(5))
+
